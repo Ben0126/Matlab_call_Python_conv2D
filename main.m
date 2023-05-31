9babfbd4cc1260dd660dc65b1c1566dc804eba9f
Ben Yeh <75458848+Ben0126@users.noreply.github.com>
2023-05-31 14:40:37 +0800

    0531

--- main.m
+++ main.m
@@ -6,22 +6,22 @@
 %         original/FFT
 
 clear;clc;
-signal = randn(10000, 10000);
-kernel = randn(6000, 6000);
+signal = randn(100, 100);
+kernel = randn(60, 60);
 
 % signal = ones(10000, 10000);
 % kernel = ones(6000, 6000);
 
 % import py libraries
 cpu_fft_conv2d = py.importlib.import_module('fft_convolve2d');
-gpu_conv2d = py.importlib.import_module('pt_gpu_convolve2d');
+gpu_conv2d = py.importlib.import_module('pt_conv2d');
 gpu_fft_conv2d = py.importlib.import_module('gpu_fft_convolve2D');
 
 
 %% First method, CPU original conv2D by python
-% tic;
-% py_result = py.scipy.signal.convolve2d(signal, kernel); 
-% toc;
+tic;
+py_result = py.scipy.signal.convolve2d(signal, kernel); 
+toc;
 
 %% Second CPU FFT conv2D by python
 tic;
@@ -30,7 +30,7 @@ toc;
 
 %% Third, GPU original conv2D by python
 tic;
-result_gpu = gpu_conv2d.pt_conv2d(signal, kernel);
+result_gpu = pt_conv2d.gpu_conv2d(signal, kernel);
 toc;
 
 %% Fourth, GPU FFT conv2D by python
