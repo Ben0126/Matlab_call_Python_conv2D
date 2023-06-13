c9f92d712400b72ef4064a12d28a218fbce62391
Ben Yeh <75458848+Ben0126@users.noreply.github.com>
2023-06-13 13:49:21 +0800

    0613

--- pt_fft_gpu_conv2d.py
+++ pt_fft_gpu_conv2d.py
@@ -1,17 +1,13 @@
-import numpy as np
 import torch
 import torch.nn.functional as F
 
 
 def pt_fft_von2d(signal,kernel):
 
-    # 将NumPy数组转换为PyTorch张量
+    # 將NumPy數轉換為PyTorch張量
     input_tensor = torch.tensor(signal, dtype=torch.double)
     kernel_tensor = torch.tensor(kernel, dtype=torch.double)
 
-    # 执行2D傅立叶变换
-    fft_result = torch.fft.fft2(input_tensor, dim=(-2, -1))
-
-    # 执行2D卷积操作
+    # 執行2D卷積操作
     conv_result = F.conv2d(input_tensor.unsqueeze(0).unsqueeze(0), kernel_tensor.unsqueeze(0).unsqueeze(0))
     return conv_result
